bfc52735d4bcee3a0f5f24a992a763b5ee1d11f3
Noveria <brucew5978@gmail.com>
2024-11-07 11:06:25 -0500

    switching to arduino nano

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,17 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(921600);
+  Serial.println("Hello from the setup");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Looping");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
