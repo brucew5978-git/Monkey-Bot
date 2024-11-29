f320812407d27a2816286221c2465e92ab0c4f0e
Noveria <brucew5978@gmail.com>
2024-11-29 14:35:45 -0500

    const LED on

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,19 @@ Servo myServo;
 // Define the PWM pin
 int servoPinL = 9;
 int servoPinR = 7;
-int ledPin = 1;
+int redLedPin1 = 2;
+int redLedPin2 = 3;
+int blueLedPin = 5;
 
 void setup() {
   // Attach the servo to the specified pin
 
-  pinMode(ledPin, OUTPUT);
+  pinMode(redLedPin1, OUTPUT);
+  pinMode(redLedPin2, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+  digitalWrite(redLedPin1, HIGH);
+  digitalWrite(redLedPin2, HIGH);
+  digitalWrite(blueLedPin, HIGH);
 }
 
 void loop() {
@@ -22,7 +29,9 @@ void loop() {
   delay(20);
   myServo.attach(servoPinR);
   myServo.write(0);
-  digitalWrite(ledPin, HIGH);
+  // digitalWrite(redLedPin1, HIGH);
+  // digitalWrite(redLedPin2, HIGH);
+  // digitalWrite(blueLedPin, HIGH);
   delay(1000);
 
   // Rotate to 90 degrees
@@ -31,7 +40,9 @@ void loop() {
   delay(20);
   myServo.attach(servoPinR);
   myServo.write(90);
-  digitalWrite(ledPin, HIGH);
+  // digitalWrite(redLedPin1, LOW);
+  // digitalWrite(redLedPin2, LOW);
+  // digitalWrite(blueLedPin, LOW);
   
   delay(1000);
 }
