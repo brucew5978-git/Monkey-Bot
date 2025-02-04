8f33006ea84118af1635d9cb1596c51bcee1da54
brucewang <brucew5978@gmail.com>
2025-02-04 01:08:27 -0500

    adding 2 servo motor control

--- servo.cpp
+++ servo.cpp
@@ -1,27 +1,27 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-// Create a Servo object
-Servo myServo;
+Servo servoL;
+Servo servoR;
 
-// Define the PWM pin
-int servoPin = 9;
+//PWM pin
+int servoPinL = 9;
+int servoPinR = 10;
 
 void setup() {
-  // Attach the servo to the specified pin
-  myServo.attach(servoPin);
+  //Attach servos to the specific pin
+  servoL.attach(servoPinL);
+  servoR.attach(servoPinR);
 }
 
 void loop() {
-  // Rotate to 0 degrees
-  myServo.write(0);
+  //Start position
+  servoL.write(0);
+  servoR.write(0);
   delay(1000);
 
-  // Rotate to 90 degrees
-  myServo.write(90);
-  delay(1000);
-
-  // Rotate to 180 degrees
-  myServo.write(180);
+  //Rotate to 90 degrees
+  servoL.write(90);
+  servoR.write(180);
   delay(1000);
 }
